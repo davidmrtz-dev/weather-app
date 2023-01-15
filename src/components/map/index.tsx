483679be79273f42501a3e466419e23e92f28f7c
David Mtz <davidelimg1@gmail.com>
2023-01-15 16:50:21 -0600

    [WIP]: Updated coordinates type.

--- src/components/map/index.tsx
+++ src/components/map/index.tsx
@@ -2,19 +2,20 @@ import {
   MapContainer,
   TileLayer,
 } from 'react-leaflet'
-import { LatLngExpression } from "leaflet";
+import { LatLngLiteral } from "leaflet";
 import "leaflet/dist/leaflet.css";
 import LocationMarker from './LocationMarker';
 
 const Map = (): JSX.Element => {
-  const defaultLocation: LatLngExpression = [19.274919, -99.147155];
+  const defaultLocation: LatLngLiteral = {lat: 19.274919, lng: -99.147155};
 
   return(
     <MapContainer
       center={defaultLocation}
       zoom={13}
-      style={{ width: 500, height: 350 }}
+      style={{ width: 500, height: 350, zIndex: 1 }}
       dragging={false}
+      scrollWheelZoom={false}
     >
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
