67f368fb05044b12791a5aa31b961a3a8e4810f4
David Mtz <davidelimg1@gmail.com>
2023-01-14 20:58:33 -0600

    [FEAT]: Implemented navbar.

--- src/components/navigation/index.tsx
+++ src/components/navigation/index.tsx
@@ -1,6 +1,24 @@
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+const NavigationContainer = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 5em;
+  background-color: ${props => props.theme.colors.brandGray};
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+`;
+
 const Navigation = (): JSX.Element => {
  return(
-  <h1>a navabar</h1>
+  <NavigationContainer>
+    <Link to='/'>Home</Link>
+    <Link to='/about'>About</Link>
+  </NavigationContainer>
  );
 };
 
