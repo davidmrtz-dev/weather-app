57837a44cd1fa072f287960904008d3828346c27
David Mtz <davidelimg1@gmail.com>
2023-01-15 00:38:13 -0600

    [WIP]. Destructured into multiple components

--- src/components/map/LocationMarker.tsx
+++ src/components/map/LocationMarker.tsx
@@ -1,15 +1,13 @@
 import {
   Marker,
   Popup,
-  LayersControl,
-  Circle,
   useMapEvents
 } from 'react-leaflet'
 import { LatLngExpression } from "leaflet";
 import "leaflet/dist/leaflet.css";
 import L from "leaflet";
 import { useState } from 'react';
-import { MapLayerColors } from '../../@types';
+import { ControledLayers } from './ControledLayers';
 
 const LocationMarker = (): null | JSX.Element => {
   const icon = new L.Icon({
@@ -37,40 +35,5 @@ const LocationMarker = (): null | JSX.Element => {
     </>
   )
 };
-
-const ControledLayers = ({ location }: { location: LatLngExpression}):JSX.Element => {
-  return(<LayersControl position="topright">
-    <ControledLayer
-      name='Layer-1'
-      location={location}
-      color='green'
-      fillColor='green'
-      radius={2200}
-      checked
-    />
-  </LayersControl>);
-};
-
-const ControledLayer = ({
-  name,
-  location,
-  color,
-  fillColor,
-  radius,
-  checked
-}: {
-  name: string;
-  location: LatLngExpression,
-  color: MapLayerColors,
-  fillColor: MapLayerColors,
-  radius: number,
-  checked?: boolean
-}): JSX.Element => <LayersControl.Overlay name={name} checked={checked}>
-  <Circle
-    center={location}
-    pathOptions={{ color, fillColor}}
-    radius={radius}
-  />
-</LayersControl.Overlay>;
 
 export default LocationMarker;
