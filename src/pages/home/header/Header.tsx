a890cd4b3340f20fba18f2896a32e86ef6671657
David Mtz <davidelimg1@gmail.com>
2023-01-16 15:58:53 -0600

    [WIP]: Implementing header content.

--- src/pages/home/header/Header.tsx
+++ src/pages/home/header/Header.tsx
@@ -1,16 +1,11 @@
-import { Typography } from "antd";
+import { Image, Typography } from "antd";
 import { LatLngLiteral } from "leaflet";
 import styled from "styled-components";
 import { LoadingMask } from "../../../atoms/LoadingMask";
+import logo from '../../../assets/svg/02n.svg';
 import { theme } from "../../../Theme";
 
 const HeaderContainer = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
-
-const WeatherDataContainer = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
@@ -28,8 +23,32 @@ export const Header = ({
   }}>
     Click on the map
   </Typography>) :
-  <WeatherDataContainer>
-    <LoadingMask width={45} height={45} />
-  </WeatherDataContainer>
+  (<HeaderContent />)
 }
 </HeaderContainer>;
+
+const HeaderContentContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+`;
+
+const HeaderContentWrapper = styled.div`
+  flex: 1;
+  margin: 5px;
+  background: gray;
+`;
+
+const HeaderContent = (): JSX.Element => <HeaderContentContainer>
+  <HeaderContentWrapper
+    style={{
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'space-around'
+    }}
+  >
+    <Typography style={{...theme.texts.brandH1}}>21</Typography>
+    <Image width={100} src={logo} />
+  </HeaderContentWrapper>
+  <HeaderContentWrapper><h1>okokokok</h1></HeaderContentWrapper>
+</HeaderContentContainer>;
