1b9ac2901df912828915d37d528141db806cbb88
David Mtz <davidelimg1@gmail.com>
2023-01-16 14:16:49 -0600

    [WIP]: Moved up state to home page.

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,8 +1,36 @@
+import { LatLngLiteral } from 'leaflet';
+import { useEffect, useState } from 'react';
+import { City } from '../../@types';
+import { getCitiesByLocation } from '../../api/core/GeoDB';
 import Map from '../../components/map';
 
 const Home = (): JSX.Element => {
+  const defaultLocation: LatLngLiteral = {lat: 19.274919, lng: -99.147155};
+  const [position, setPosition] = useState<null | LatLngLiteral>(null);
+  const [nearCities, setNearCities] = useState<City []>([]);
+
+  const getCities = async(location: LatLngLiteral): Promise<void> => {
+    const result = await getCitiesByLocation(location, '100');
+    setNearCities(result.data);
+  };
+
+  useEffect(() => {
+    if (position) {
+      getCities(position);
+    }
+  }, [position]);
+
+  useEffect(() => {
+    console.log('nearcities:', nearCities);
+  }, [nearCities]);
+
   return (
-    <Map />
+    <Map
+      defaultLocation={defaultLocation}
+      position={position}
+      nearCities={nearCities}
+      setPosition={setPosition}
+    />
   );
 };
 
