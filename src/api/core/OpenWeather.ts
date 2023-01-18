dcfff7ba9ec617aa29fd386b8ae0d29d562187b3
David Mtz <davidelimg1@gmail.com>
2023-01-18 12:29:23 -0600

    [FEAT]: Implemented error handling for weahter clal.

--- src/api/core/OpenWeather.ts
+++ src/api/core/OpenWeather.ts
@@ -3,7 +3,7 @@ import { WeatherResponse } from '../../@types';
 
 export const getWeatherByLocation = async (location: LatLngLiteral): Promise<WeatherResponse> => {
   const response = await fetch(
-    `https://api.openweathermap.org/data/2.5/weaher?lat=${location.lat}&lon=${location.lng}&appid=c7f8ceb3a741a54e536808e9a71a51ca`
+    `https://api.openweathermap.org/data/2.5/weather?lat=${location.lat}&lon=${location.lng}&appid=c7f8ceb3a741a54e536808e9a71a51ca`
   );
   const data = await response.json();
   if (data.message) throw Error;
